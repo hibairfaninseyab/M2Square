ec88c70cce60b55d7b5d21a741a689a2cb77cc92
hibairfaninseyab <hiba.irfan@inseyab.com>
2026-03-16 15:06:42 +0500

    mapped finance page and buy sell

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Footer from './component/Footer/Footer'
 import Header from './component/header/header'
 import Hero from './component/landing/Hero'
+import FinancePage from './component/finance/finanace'
+import BuySellPage from './component/buysell/BuySell'
 
 function App() {
   const [count, setCount] = useState(0)
